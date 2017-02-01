02a3bfb0dadb7672ef00a421de76ec6cf3c9ae14
Teerawat kumsila <teerawatkumsila@Teerawats-Mac.local>
2017-02-01 02:45:50 -0800

    e

--- lab4.1/FirstViewController.swift
+++ lab4.1/FirstViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController , UITextViewDelegate , UITableViewDataSource {
 
+    let x = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UserDefaults.standard.set("oommi", forKey: "name")
+        let x = UserDefaults.standard.object(forKey: "name")
+        print(x)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "asdsad"
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "view control", sender: self)
+    }
+   
+    
 }
 
--- lab4.1/SecondViewController.swift
+++ lab4.1/SecondViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController , UITextFieldDelegate {
+    
+    let y = Int()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
